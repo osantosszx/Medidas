8be40a0e89046d1c69b121dd5224f0e02272f44b
guilherme santos <guiss250507@gmail.com>
2025-10-03 22:08:03 -0300

    realizar atividade medidas

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double m, cm, km;
+Console.Write("Entre com a medida (em metros):");
+
+
+m = Convert.ToDouble(Console.ReadLine());
+cm = m * 100;
+km = m / 1000;
+Console.WriteLine("---equivalente---");
+Console.Write(cm);
+Console.WriteLine(" cm");
+Console.Write(m);
+Console.WriteLine(" m");
+Console.Write(km);
+Console.WriteLine(" km");
+
